027f784943a819f1c144e9239ed58ebfd42ec779
sasankaweera123 <71062887+sasankaweera123@users.noreply.github.com>
2021-07-20 14:58:12 +0530

    Updated

---
+++ pythonUIDesign/ui/scratch.py
@@ -0,0 +1,22 @@
+import turtle
+
+turtle.bgcolor("black")
+turtle.pensize(2)
+turtle.speed(0)
+turtle.pencolor("green")
+
+
+def drawCircle(radius):
+    for i in range(5):
+        turtle.circle(radius)
+        radius -= 10
+
+
+def drawDesign():
+    for i in range(10):
+        drawCircle(150)
+        turtle.right(36)
+
+
+drawDesign()
+turtle.exitonclick()
